f23ce58be4f0daa85ed3a13addb4b71a4b80df04
juan Moreno <jmoreno@cigip.upv.es>
2023-04-02 23:24:41 +0200

    wip

--- src/vrp_heuristic.jl
+++ src/vrp_heuristic.jl
@@ -14,21 +14,48 @@ using ProfileView
 using Base.Threads
 using Random
 using LoopVectorization
-function shuffled_list(n::Int64)
+# function shuffled_list(n::Int64)
+#     beta=10
+#     c_list=collect(1:n)
+#     o_list=Vector{Int64}(undef,n)
+#     k=0
+#     index1=rand(Geometric(0.2),length(c_list))
+#
+#     for j in 1:length(c_list)
+#         k+=1
+#         index=index1[k] % length(c_list)+1
+#         o_list[k]=c_list[index]
+#         deleteat!(c_list,index)
+#     end
+#     return o_list
+# end
+function shuffled_list_knuth(n::Int64)
     beta=10
     c_list=collect(1:n)
-    o_list=Vector{Int64}(undef,n)
     k=0
-    index1=rand(Geometric(0.2),length(c_list))
+    index1=rand(Geometric(0.8), n)
 
     for j in 1:length(c_list)
+
         k+=1
-        index=index1[k] % length(c_list)+1
-        o_list[k]=c_list[index]
-        deleteat!(c_list,index)
+        index=index1[k] % (length(c_list)-k+1)+1
+        @inbounds c_list[k],c_list[index]=c_list[index],c_list[k]
     end
-    return o_list
+    return c_list
 end
+
+function knuth_shuffle_geometric(n::Int)
+    a = collect(1:n)
+    for i in 1:n
+        j = i + rand(Geometric(0.2))
+        if j > n
+            j = n
+        end
+        a[i], a[j] = a[j], a[i]
+    end
+    return a
+end
+
 function read_data(filename)
 
 # with open(fileName) as instance:g
@@ -142,82 +169,12 @@ end
 
 
 
-function CWS(solution::Solution, nodes::Vector{Node}, OrderedEdges, DictNodeToRoute, DCostNodes,S, D)
-    vehCap= 100
-    for k in shuffled_list(length(OrderedEdges))
-    #     # select the next edge from the list
-    #     ijEdge = savingsList.pop(0) # select the next edge from the list
-         @inbounds i1=OrderedEdges[k][1]
-         @inbounds i2=OrderedEdges[k][2]
-         @inbounds iNode = nodes[i1]
-         @inbounds jNode = nodes[i2]
-
-        iNR1=iNode.route
-        iNR2=jNode.route
-    #     # determine the routes associated with each node
-        @inbounds iRoute = solution.routes[iNR1]
-        @inbounds jRoute = solution.routes[iNR2]
-    #     # check if merge is possible
-        isMergeFeasible = checkMergingConditions(iNode, jNode, iRoute, jRoute,vehCap)
-    #     # if all necessary conditions are satisfied, merge
-        if isMergeFeasible == true
-
-            # deterimine if iNode is at the beginning or end of iRoute
-            iNodeBTrue = iRoute.nodes[1].ID == iNode.ID
-            # determine if jNode is at the beginning or end of jRoute
-            jNodeBTrue = jRoute.nodes[1].ID == jNode.ID
-
-            if iNodeBTrue==jNodeBTrue
-                if iNodeBTrue==true
-                    reverse!(iRoute.nodes)
-                else
-                    reverse!(jRoute.nodes)
-                end
-            else
-                if iNodeBTrue==true
-                    reverse!(iRoute.nodes)
-                    reverse!(jRoute.nodes)
-                end
-            end
-            if length(iRoute.nodes) > 1
-               iNode.isInterior = true
-            end
-            if length(jRoute.nodes) > 1
-               jNode.isInterior = true
-            end
-
-
-            for n in jRoute.nodes
-                n.route= iNode.route
-#                 DictNodeToRoute[jRoute.nodes[i].ID] = DictNodeToRoute[iRoute.nodes[1].ID]
-
-            end
-
-            iRoute.nodes = vcat(iRoute.nodes, jRoute.nodes)
-            jRoute.nodes= []
-            iRoute.demand += jRoute.demand
-            iRoute.cost += jRoute.cost - DCostNodes[iNode.ID ] - DCostNodes[jNode.ID] + D[iNode.ID, jNode.ID]
-
-
-
-
-
-        end
-    end
-    solution.cost= 0
-    for i in 1:length(solution.routes)
-        if length(solution.routes[i].nodes) > 0
-            solution.cost += solution.routes[i].cost
-        end
-    end
-
-end
 
 
 function CWS(solution::Solution, nodes::Vector{Node}, OrderedEdges, DictNodeToRoute, DCostNodes,S, D, best_sol)
     vehCap= 100
 
-    for k in shuffled_list(length(OrderedEdges))
+    for k in shuffled_list_knuth(length(OrderedEdges))
 #         k+=rand((-5:5))
 #         if k<0:
 #             k=1
@@ -290,10 +247,10 @@ function CWS(solution::Solution, nodes::Vector{Node}, OrderedEdges, DictNodeToRo
             solution.cost += solution.routes[i].cost
         end
     end
-    if solution.cost<best_sol.cost
-        best_sol=solution
-    end
+    if solution.cost<best_sol
+        best_sol=solution.cost
 
+    end
     return best_sol
 
 end
@@ -373,37 +330,45 @@ end
 
 
 
-function main()
+function main(instanceName)
 
 #     instanceName = "A-n80-k10" # name of the instance
-    instanceName = "A-n32-k5" # name of the instance
 
-    fileName = "data/" * instanceName * "_input_nodes.txt"
+    fileName = "data/" * instanceName
     solution, nodes, OrderedEdges, DictNodeToRoute, DCostNodes,S ,D ,routes, cost = read_data(fileName)
-    num_routes=10000
+    num_routes=10000*3*4
     Edges=Vector{typeof(OrderedEdges)}(undef, 2000000)
 
     Solutions=Vector{Solution}(undef, num_routes)
 
-    for k in 1:num_routes
-        Solutions[k]=deepcopy(Solution(0,  routes, cost,nodes ))
+    best_sol=deepcopy(Solution(0,  deepcopy(routes), cost,deepcopy(nodes) ))
 
-    end
 
 #  for k in 1:num_routes
 #         Solutions[k]=Solution(0,  deepcopy(routes), cost,deepcopy(nodes ))
 #
 #     end
 
-    best_sol=Solutions[1]
-
+#     best_sol=Solutions[1]
+    best_sol_cost=100000000
     for k in 1:num_routes
 
-        best_sol=CWS(Solutions[k], Solutions[k].nodes, OrderedEdges, DictNodeToRoute, DCostNodes,S,D,best_sol
-        )
-   end
-   println(best_sol.cost)
+        best_sol_cost=CWS(solution, solution.nodes, OrderedEdges,
+         DictNodeToRoute, DCostNodes,S,D,best_sol_cost  )
 
+         for node in solution.nodes[2:end]
+            node.isInterior=false
+            node.route=node.ID-1
+            solution.routes[node.route].nodes=Node[node]
+            solution.routes[node.route].demand=node.demand
+            solution.routes[node.route].cost=2*D[1,node.ID]
+            DictNodeToRoute[node.ID] = node.ID-1
+            solution.cost=0
+
+        end
+#
+   end
+   println(best_sol_cost)
 #     best_sols=[Solutions[l] for l in 1:Threads.nthreads()]
 #     thread_count=[0 for l in 1:Threads.nthreads()]
 #     @threads for k in 1:num_routes
@@ -415,12 +380,41 @@ function main()
 
 #    println([b.cost for b in best_sols])
 #    println(thread_count)
+#
+#   best_sol_costs=[100000000 for _ in 1:Threads.nthreads()]
+#     @threads for k in 1:num_routes
+#
+#         best_sol_costs[Threads.threadid()]=CWS(solution, solution.nodes, OrderedEdges,
+#          DictNodeToRoute, DCostNodes,S,D,best_sol_cost  )
+#
+#          for node in solution.nodes[2:end]
+#             node.isInterior=false
+#             node.route=node.ID-1
+#             solution.routes[node.route].nodes=Node[node]
+#             solution.routes[node.route].demand=node.demand
+#             solution.routes[node.route].cost=2*D[1,node.ID]
+#             DictNodeToRoute[node.ID] = node.ID-1
+#             solution.cost=0
+#
 
+#         end
+# #
+#    end
+#    println(best_sol_cost)
 end
 
-@time main()
-@profview main()
-readline()
+for filename in filter(x -> occursin(r"\.txt$", x), readdir("data"))[1:1]
+    println(filename)
+       @time main(filename)
+
+   @profview main(filename)
+    readline()
+end
+
+#
+# @time main()
+#  main()
+# readline()
 # b=@benchmark main() seconds=1 time_tolerance=0.01 gctrial=true samples=1000 evals=1
 #
 # println("Benchmarking vrp_heuristic.jl")
